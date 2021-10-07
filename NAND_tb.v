1bbec81e4c4c12d39ad190db1350f10777eb4637
Rupal Gupta <52855649+rupal13g@users.noreply.github.com>
2021-10-07 10:05:27 +0530

    Create NAND_tb.v

---
+++ NAND_tb.v
@@ -0,0 +1,20 @@
+module nand_tb;
+  reg ta, tb;
+  wire ty;
+  nandgate nag(.a(ta), .b(tb), .y(ty));
+  initial
+    begin
+      $monitor (ta, tb, ty);
+      ta = 1'b0;
+      tb = 1'b0;
+      #10
+      ta = 1'b0;
+      tb = 1'b1;
+      #10
+      ta = 1'b1;
+      tb = 1'b0;
+      #10
+      ta = 1'b1;
+      tb = 1'b1;
+    end
+endmodule
